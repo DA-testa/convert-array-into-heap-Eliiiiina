e58e1cbea6710781950765f66cffb8ca18b7af44
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-03-15 08:29:54 +0000

    tests

--- build_heap.py
+++ build_heap.py
@@ -5,10 +5,26 @@ def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    n= len(data)
+    for i in range(n//2-1, -1, -1):
+        swaps += sift_down(data,i)
     return swaps
 
+def sift_down(data,i):
+    swaps = []
+    n = len(data)
+    min_index = i
+    left_child = 2 * i + 1
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
+    right_child = 2 * i + 2
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        swaps += sift_down(data, min_index)
+    return swaps
 
 def main():
     
