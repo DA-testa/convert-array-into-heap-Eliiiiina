255a7262b8cd09a6b2ac6647e9f5501c72b9c115
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-03-19 22:42:48 +0000

    tst

--- build_heap.py
+++ build_heap.py
@@ -30,7 +30,7 @@ def main():
         assert len(data) == m 
     elif "F" in ievade: 
         fails = input() 
-        atrasanas = './tests/' 
+        atrasanas = './tests' 
         faila_vieta = os.path.join(atrasanas, fails) 
         with open(faila_vieta, mode="r") as file: 
             m = int(file.readline()) 
@@ -40,6 +40,6 @@ def main():
     for i, j in swaps: 
         print(i, j) 
         
-        
+
 if __name__ == "_main_":
     main()
