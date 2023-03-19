0cba10f3053a2cdf84fff24774039cf58bd0d3f2
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-03-19 23:08:57 +0000

    tests

--- build_heap.py
+++ build_heap.py
@@ -1,46 +1,44 @@
 # python3
 #Elīna Miltiņa 221RDC017 18.grupa
-
 import os
-def build_heap(data):
-    swaps = []
-    n = len(data)
-    for i in range(n//2,-1,-1):
-        shift(data, i, swaps)
-    return swaps
 
-def shift(data, i, swaps):
-    n = len(data)
+def build_heap(data, swaps, i, n):
+    left_child = 2 * i + 1
+    right_child = 2 * i + 2
     min_index = i
-    left_c = 2 * i + 1
-    right_c = 2 * i + 2
-    if left_c < n and data[left_c] < data[min_index]:
-        min_index = left_c
-    if right_c < n and data[right_c] < data[min_index]:
-        min_index = right_c
-    if min_index != i:
-        swaps.append((i, min_index))
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+    if i != min_index:
         data[i], data[min_index] = data[min_index], data[i]
-        shift(data, min_index, swaps)
+        swaps.append((i, min_index))
+        build_heap(data, swaps, min_index, n)
 
 def main():
-    ievade = input("I or F")
-    if "I" in ievade:
-        m = int(input())
-        data = list(map(int, input().split()))
-        assert len(data) == m
-    elif "F" in ievade:
-        fails = input()
+    ievade = input("I or F: ")
+    if "I" in ievade.upper():
+        n = int(input("Enter the number of elements: "))
+        data = list(map(int, input("Enter the elements: ").split()))
+        assert len(data) == n
+    elif "F" in ievade.upper():
+        fails = input("Enter the file name: ")
         atrasanas = './tests/'
         faila_vieta = os.path.join(atrasanas, fails)
         with open(faila_vieta, mode="r") as file:
-            m = int(file.readline())
+            n = int(file.readline())
             data = list(map(int, file.readline().split()))
-    swaps = build_heap(data)
+    else:
+        print("Invalid input. Please enter 'I' or 'F'.")
+        return
+    
+    swaps = []
+    for i in range(n // 2 - 1, -1, -1):
+        build_heap(data, swaps, i, n)
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
 
-
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
+
